d077df13a9db69b84505601aae9ddc2fe9e3d54c
reuben-j <reubenjarvela@gmail.com>
2024-08-26 16:39:33 +0100

    FirstCommit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() { // this function is called when button 'run' is hit
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(    
+        appBar: AppBar(), 
+      ),
+    );
+  }
+}
