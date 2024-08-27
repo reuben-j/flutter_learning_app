9f0688d5ff2d3743854ab3e0888bfff4b15103d1
reuben-j <reubenjarvela@gmail.com>
2024-08-27 13:48:11 +0100

    PageTitle

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(    
         appBar: AppBar(
-          title: Text("Hello"),
+          title: Text("Page Title"),
         ), 
       ),
     );
