ba2480d4cb9b5b53cf174ca3d1a76bc117dac8c9
reuben-j <reubenjarvela@gmail.com>
2024-09-02 10:56:09 +0100

    NavWorking:)

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,75 @@
 import 'package:flutter/material.dart';
 
-// https://api.flutter.dev/flutter/material/Icons-class.html
-// flutter icons^^
-
 void main() {
-  // this function is called when button 'run' is hit
-  runApp(MyApp());
+  runApp(const nap_app());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+// ignore: camel_case_types
+class nap_app extends StatefulWidget {
+  const nap_app({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  State<nap_app> createState() => _nap_appState();
 }
 
-class _MyAppState extends State<MyApp> {
-  String button_name = "Click";
-  String button2 = "Nice one!";
-  int currentIndex = 1;
+class _nap_appState extends State<nap_app> {
 
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Page Title"),
-          backgroundColor: const Color.fromARGB(255, 93, 153, 255),
-        ),
-        body: Center(
-          // if else statement to change what view is seen depending on the index
-          child: currentIndex == 0 ? Container(
-            height: double.infinity,
-            width: double.infinity,
-            color: const Color.fromARGB(255, 216, 255, 254),
-            
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              //crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-                    backgroundColor: Color.fromARGB(255, 162, 0, 255),
-                  ),
-                    onPressed: () {
-                      setState(() {
-                        button_name = "Yeiw";
-                      });
-                    },
-                    child: Text(button_name),),
-                    ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        button_name = "Yeiw";
-                      });
-                    },
-                    child: Text("Nav"),),
-                    ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-                    backgroundColor: Color.fromARGB(255, 162, 0, 255),
-                  ),
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext){
-                        return const pagetwo();
-                      },));
-                    },
-                    child: Text(button_name),)
-              ],
-            ), 
-          ) : Image.asset('images/PEAPrac1.png')
-          
-
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-                label: "Home",
-                backgroundColor: Colors.black,
-                icon: Icon(Icons.home, color: Colors.black, size: 24)),
-            BottomNavigationBarItem(
-                label: "Settings Tab",
-                icon: Icon(Icons.settings, color: Colors.black, size: 24))
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index){
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
-      ),
+    return const MaterialApp(
+      home: firstPage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class pagetwo extends StatelessWidget {
-  const pagetwo({super.key});
+class firstPage extends StatefulWidget {
+  const firstPage({super.key});
+
+  @override
+  State<firstPage> createState() => _firstPageState();
+}
+
+class _firstPageState extends State<firstPage> {
+  String buttonName = "Yep";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("First Page"),
+        ),
+        body: Center(
+          child: Container(
+            height: 240,
+            width: 300,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                foregroundColor: const Color.fromARGB(255, 1, 1, 1),
+                backgroundColor: const Color.fromARGB(255, 30, 255, 0)
+              ), onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context){
+                  return const secondPage();
+                }));
+              },
+              child: Text(buttonName),
+            ),
+          ),
+        ), 
+        
+      );
+  }
+}
+
+class secondPage extends StatelessWidget {
+  const secondPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Second Page")
+      ),
+    );
   }
 }
